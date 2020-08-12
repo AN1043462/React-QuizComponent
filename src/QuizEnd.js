d48b6f3a59b2c96555d41507c944ee9076b9a434
AN1043462 <34911333+AN1043462@users.noreply.github.com>
2020-08-12 17:42:14 +0530

    Update QuizEnd.js
    Updated QuizEnd Component

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
-import './App.css'
 
 class QuizEnd extends Component {
+  handleResetClick() {
+    this.props.resetClickHandler()
+  }
   render() {
     return (
-    <div>
+      <div>
         <p>Thanks for playing!</p>
-        <a href=''>Reset Quiz</a>
-      </div>
+        <a href='' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
+      </div>      
     )
   }
 }
